2b140522051b241d42d33086ca123ea6be5d8178
Ital023 <contadoitalo911@gmail.com>
2024-10-19 22:24:53 -0300

    feat: Resource Not Found Exception Handler

---
+++ src/main/java/com/devsuperior/demo/controllers/exceptions/ControllerExceptionHandler.java
@@ -0,0 +1,34 @@
+package com.devsuperior.demo.controllers.exceptions;
+
+import com.devsuperior.demo.services.exceptions.ResourceNotFoundException;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.Instant;
+
+@RestControllerAdvice
+public class ControllerExceptionHandler {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<StandartError> entityNotFound(ResourceNotFoundException e,
+                                                        HttpServletRequest request) {
+
+        HttpStatus status = HttpStatus.NOT_FOUND;
+
+        StandartError error = new StandartError();
+        error.setTimestamp(Instant.now());
+        error.setStatus(status.value());
+        error.setError("Resource Not Found");
+        error.setMessage(e.getMessage());
+        error.setPath(request.getRequestURI());
+
+
+        return ResponseEntity.status(status).body(error);
+    }
+
+
+
+}
