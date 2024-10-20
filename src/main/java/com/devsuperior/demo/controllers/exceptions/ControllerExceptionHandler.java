9c51fa274a1b47d3452cda96f336c87ebeb80c57
Ital023 <contadoitalo911@gmail.com>
2024-10-19 22:25:59 -0300

    feat: databaseResource Not Found Exception Handler

--- src/main/java/com/devsuperior/demo/controllers/exceptions/ControllerExceptionHandler.java
+++ src/main/java/com/devsuperior/demo/controllers/exceptions/ControllerExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.devsuperior.demo.controllers.exceptions;
 
+import com.devsuperior.demo.services.exceptions.DataBaseException;
 import com.devsuperior.demo.services.exceptions.ResourceNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
@@ -13,7 +14,7 @@ import java.time.Instant;
 public class ControllerExceptionHandler {
 
     @ExceptionHandler(ResourceNotFoundException.class)
-    public ResponseEntity<StandartError> entityNotFound(ResourceNotFoundException e,
+    public ResponseEntity<StandartError> entityNotFound(DataBaseException e,
                                                         HttpServletRequest request) {
 
         HttpStatus status = HttpStatus.NOT_FOUND;
@@ -22,6 +23,23 @@ public class ControllerExceptionHandler {
         error.setTimestamp(Instant.now());
         error.setStatus(status.value());
         error.setError("Resource Not Found");
+        error.setMessage(e.getMessage());
+        error.setPath(request.getRequestURI());
+
+
+        return ResponseEntity.status(status).body(error);
+    }
+
+    @ExceptionHandler(DataBaseException.class)
+    public ResponseEntity<StandartError> database(ResourceNotFoundException e,
+                                                        HttpServletRequest request) {
+
+        HttpStatus status = HttpStatus.NOT_FOUND;
+
+        StandartError error = new StandartError();
+        error.setTimestamp(Instant.now());
+        error.setStatus(status.value());
+        error.setError("Database Exception");
         error.setMessage(e.getMessage());
         error.setPath(request.getRequestURI());
 
