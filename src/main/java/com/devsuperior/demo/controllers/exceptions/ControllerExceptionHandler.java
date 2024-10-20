6263ba6a64a9f7c208fc72cf9c80d3baced2f516
Ital023 <contadoitalo911@gmail.com>
2024-10-19 22:34:22 -0300

    feat: MethodArgumentNotValidException handler

--- src/main/java/com/devsuperior/demo/controllers/exceptions/ControllerExceptionHandler.java
+++ src/main/java/com/devsuperior/demo/controllers/exceptions/ControllerExceptionHandler.java
@@ -5,6 +5,8 @@ import com.devsuperior.demo.services.exceptions.ResourceNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -34,7 +36,7 @@ public class ControllerExceptionHandler {
     public ResponseEntity<StandartError> database(ResourceNotFoundException e,
                                                         HttpServletRequest request) {
 
-        HttpStatus status = HttpStatus.NOT_FOUND;
+        HttpStatus status = HttpStatus.BAD_REQUEST;
 
         StandartError error = new StandartError();
         error.setTimestamp(Instant.now());
@@ -43,6 +45,26 @@ public class ControllerExceptionHandler {
         error.setMessage(e.getMessage());
         error.setPath(request.getRequestURI());
 
+
+        return ResponseEntity.status(status).body(error);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ValidationError> database(MethodArgumentNotValidException e,
+                                                  HttpServletRequest request) {
+
+        HttpStatus status = HttpStatus.UNPROCESSABLE_ENTITY;
+
+        ValidationError error = new ValidationError();
+        error.setTimestamp(Instant.now());
+        error.setStatus(status.value());
+        error.setError("Validation error");
+        error.setMessage(e.getMessage());
+        error.setPath(request.getRequestURI());
+
+        for(FieldError fieldError : e.getBindingResult().getFieldErrors()) {
+            error.addError(fieldError.getField(), fieldError.getDefaultMessage());
+        }
 
         return ResponseEntity.status(status).body(error);
     }
