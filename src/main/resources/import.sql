4dcdb8e7ca9f617886bbaad53eda213307ec05ae
Ital023 <contadoitalo911@gmail.com>
2024-10-19 22:10:08 -0300

    feat: Seeding sql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,3 +1,13 @@
+INSERT INTO tb_user (name, email, password) VALUES ('Ana', 'ana@gmail.com', '$2a$10$mZfx.5Tv8nUq.2AYeA6fxOc/t71nb02E/yzhs233G1gYvKqo5fEHG');
+INSERT INTO tb_user (name, email, password) VALUES ('Bob', 'bob@gmail.com', '$2a$10$mZfx.5Tv8nUq.2AYeA6fxOc/t71nb02E/yzhs233G1gYvKqo5fEHG');
+
+INSERT INTO tb_role (authority) VALUES ('ROLE_OPERATOR');
+INSERT INTO tb_role (authority) VALUES ('ROLE_ADMIN');
+
+INSERT INTO tb_user_role (user_id, role_id) VALUES (1, 1);
+INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 1);
+INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 2);
+
 INSERT INTO tb_department(name) VALUES ('Sales');
 INSERT INTO tb_department(name) VALUES ('Management');
 INSERT INTO tb_department(name) VALUES ('Training');
